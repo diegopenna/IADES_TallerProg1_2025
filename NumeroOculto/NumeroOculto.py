3cdf2c5a753cc287980410857f50f0c345090cc8
Diego <diegopenna@gmail.com>
2025-08-25 21:23:06 -0300

    Numero Oculto - Mejora de codigo

--- NumeroOculto/NumeroOculto.py
+++ NumeroOculto/NumeroOculto.py
@@ -8,20 +8,10 @@ numeroOculto = numeroOculto[-4:]
 print()
 print(numeroOculto)
 print()
+
 numeroInvalido = False
 for i in range(3):
-    for j in range(i + 1, 4):
-        #print("Comparar digito", i , "con digito", j) 
-        if (numeroOculto[i] == numeroOculto[j]):
-            numeroInvalido = True
-            break
-
-if (numeroInvalido):
-    print("Numero incorrecto")
-
-#otra manera
-numeroInvalido = False
-for i in range(3):
+    print(numeroOculto[i+1:])
     if (numeroOculto[i] in numeroOculto[i+1:]):
         numeroInvalido = True
         break
