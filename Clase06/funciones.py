bd11411b84716cadea26e7b9f6d38607cb546654
Diego <diegopenna@gmail.com>
2025-09-16 19:37:22 -0300

    Lubrerias externas. Y gitignore

---
+++ Clase06/funciones.py
@@ -0,0 +1,6 @@
+
+def suma(numero1, numero2):
+    return numero1 + numero2
+
+def resta(numero1, numero2):
+    return numero1 - numero2
---
+++ Clase06/principal.py
@@ -0,0 +1,6 @@
+import funciones as fn
+
+suma = fn.suma(12, 34)
+print(suma)
+resta = fn.resta(34, 12 )
+print(resta)
