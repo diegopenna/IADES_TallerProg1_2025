5cc15ba7e09b915e4b556f988c00db6d65f6898d
Diego <diegopenna@gmail.com>
2025-10-20 21:53:57 -0300

    Clases y Objetos. Clase Control Remoto

--- Clase11_objetos/ControlRemoto.py
+++ Clase11_objetos/ControlRemoto.py
@@ -1,10 +1,44 @@
 class ControlRemoto:
-    def __init__(self):
-        self.cantidadBotones = 0
-        self.marca = ""
-        self.pilas = ""
+    def __init__(self, cantidadBotones:int = 0, marca:str = "", pilas:str = ""):
+        self.cantidadBotones = cantidadBotones
+        self.marca = marca
+        self.pilas = pilas
+        self.ancho = 0.0
+        self.largo = 0.0
 
-miControl = ControlRemoto()
-miControl.cantidadBotones = 15
-miControl.marca = "BGH"
-miControl.pilas = "2xAA"
+    def __str__(self):
+        return "Control Remoto: " + self.descripcion()
+    
+    def descripcion(self):
+        retorno = ""
+        retorno = retorno + "Marca: " + str(self.marca)
+        retorno = retorno + ", CantBotones:" + str(self.cantidadBotones)
+        retorno = retorno + ", Pilas:" + str(self.pilas)
+        retorno = retorno + ", Ancho:" + str(self.ancho)
+        retorno = retorno + ", Largo:" + str(self.largo)
+        
+        return retorno
+    
+
+
+miControl = ControlRemoto(15, "BGH", "2xAAA")
+#miControl.cantidadBotones = 15
+#miControl.marca = "BGH"
+#miControl.pilas = "2xAA"
+
+print("Marca:", miControl.marca, "CantBotones:", miControl.cantidadBotones, "Pilas:", miControl.pilas, "Ancho", miControl.ancho, "Largo", miControl.largo)
+
+otroControl = ControlRemoto(marca="Samsung")
+otroControl.cantidadBotones = 20
+#otroControl.marca = "Samsung"
+
+print("Marca:", otroControl.marca, "CantBotones:", otroControl.cantidadBotones, "Pilas:", otroControl.pilas)
+
+print("\nAhora uso el metodo descripcion:")
+print(miControl.descripcion())
+print(otroControl.descripcion())
+
+
+print("\nPrint directo de la clase")
+print(miControl)
+print(otroControl)
